ce9264e421f4d8732ae14ff8524ce61e6ce55804
gareyflee <46618024+gareyflee@users.noreply.github.com>
2019-02-26 20:09:09 -0700

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,8 +1,28 @@
-#define TIMER0_IRQ_HANDLER				TIMER0_IRQHandler  
+#define TIMER0_IRQ_HANDLER				    TIMER0_IRQHandler  
 #define TIMER0_INTERRUPT_NVIC_NAME		TIMER0_IRQn      
+#define TIMER_0_MATCH                 100
 
-#define TIMER1_IRQ_HANDLER				TIMER1_IRQHandler
-#define TIMER1_INTERRUPT_NVIC_NAME		TIMER1_IRQn
+#define TRUE 				                  1
+#define FALSE				                  0
 
-#define TRUE 				1
-#define FALSE				0
+
+void TIMER0_IRQHandler(void){
+	Chip_TIMER_Disable(LPC_TIMER0);		  	// Stop TIMER0
+	Chip_TIMER_Reset(LPC_TIMER0);		 	// Reset TIMER0
+	Chip_TIMER_ClearMatch(LPC_TIMER0,0);  	// Clear TIMER0 match
+	Chip_TIMER_Enable(LPC_TIMER0);			// Enable Timer0
+}
+
+void Initialize_Timers(){
+	// Initialize Timer0
+	Chip_TIMER_Init(LPC_TIMER0);
+	Chip_TIMER_PrescaleSet(LPC_TIMER0,PRESCALE_VALUE_0);
+	Chip_TIMER_SetMatch(LPC_TIMER0,0,TIMER_0_MATCH);
+	Chip_TIMER_MatchEnableInt(LPC_TIMER0, 0);
+  
+
+	// Enable interrupts in the NVIC for the timers
+	NVIC_ClearPendingIRQ(TIMER0_INTERRUPT_NVIC_NAME);
+	NVIC_EnableIRQ(TIMER0_INTERRUPT_NVIC_NAME);
+}
+  
