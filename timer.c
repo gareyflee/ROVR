ce72ce9551971f4ca442ab6d78e343fc1ea46371
gareyflee <46618024+gareyflee@users.noreply.github.com>
2019-02-26 19:59:31 -0700

    Create timer.c

---
+++ timer.c
@@ -0,0 +1,8 @@
+#define TIMER0_IRQ_HANDLER				TIMER0_IRQHandler  
+#define TIMER0_INTERRUPT_NVIC_NAME		TIMER0_IRQn      
+
+#define TIMER1_IRQ_HANDLER				TIMER1_IRQHandler
+#define TIMER1_INTERRUPT_NVIC_NAME		TIMER1_IRQn
+
+#define TRUE 				1
+#define FALSE				0
