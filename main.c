9b8c6308ac182d3b235a9703459c12b2f92eccbf
Matthew Dupree <mattjdupree@gmail.com>
2019-03-02 16:18:34 -0800

    Add minimum build skeleton.

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+//	ECE 153B W19 ROVR Project
+#include "board.h"
+#include "chip.h"
 
+/**
+ * @brief Runs board init functions included in every project
+ */
+void Initialize_Board(){
+	SystemCoreClockUpdate();
+	Board_Init();
+}
 
 int main(){
-  printf("Garey Rules\n\n");
+	Initialize_Board();
+	printf("Garey Rules\n\n");
 
+	return 0;
 }
