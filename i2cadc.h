a2254e1323514835f49fc73c7d5a184a89b1b731
Matthew Dupree <mattjdupree@gmail.com>
2019-03-12 18:39:08 -0700

    Rework ADC read interface

--- i2cadc.h
+++ i2cadc.h
@@ -7,13 +7,15 @@
 #define I2C_SPEED			400000
 #define I2C_ADC_ADDR		0x48
 
-#define ADC_CONFIG_LEN		3 // Length of ADC config packet in bytes, including config register set
+#define ADC_CONFIG_LEN		3			// Length of ADC config packet in bytes, including config register set
 
 #define ADC_READ_TIMER 		LPC_TIMER1
-#define ADC_TIMER_PRESCALE	1200 		// Clock cycle / 1000 (set to 1 microsecond increments)
-#define ADC_READ_PERIOD		303 		// In microseconds
+#define ADC_PRESCALE_VALUE	12000		// Clock cycle / 10000 (set to 1/10 ms increments)
+#define ADC_READ_PERIOD		3033 		// In microseconds/10
+#define ADC_IRQ_HANDLER		TIMER0_IRQHandler
+#define ADC_IRQ_NVIC_NAME	TIMER0_IRQn
 
-volatile extern uint8_t ADC_isRead = False;
+#define MAX_ADC_SAMP_BUFF 512
 
 /**
  * @brief Sets up I2C0 and ADC_READ_TIMER for adc operation
@@ -26,7 +28,6 @@ void Initialize_I2CADC();
  */
 void Config_ADC(uint8_t mux);
 
-#define MAX_ADC_SAMP_BUFF 1024
 
 /**
  * @brief Reads samples single upper bytes from ADC conversion register
@@ -36,5 +37,15 @@ void Config_ADC(uint8_t mux);
  * Returned buffer will be overwritten
  */
 uint8_t* Read_ADC(uint16_t samples);
+
+/**
+ * @brief checks whether the read op is done yet
+ */
+uint8_t ADC_Read_Complete();
+
+/**
+ * @brief returns how many bytes have been read
+ */
+uint8_t ADC_Bytes_Read();
 
 #endif //I2C_H
