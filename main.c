c208b083d172b5c9a006ba7887e1233103252eb6
Matthew Dupree <mattjdupree@gmail.com>
2019-03-18 20:56:38 -0700

    Prepare mvp code.

--- main.c
+++ main.c
@@ -42,8 +42,12 @@ float Read_Mic(uint8_t mux_num){
 	return sample_avg;
 }
 
+#define NUM_MICS 4
+
 //Forward/backward, right/left
-static float vector_arr[4];
+static float vector_arr[2] = {0.0f,0.0f};
+//Forward positive, right positive
+
 /**
  * @brief takes a0,a1,a2,a3 arranged clockwise at 45 degree points, sets vector_arr
  * @param a float array a0,a1,a2,a3
@@ -54,36 +58,61 @@ void vectorMath(const float* a){
 	//return vector_differences;
 }
 
+#define MOTOR_SPEED 64
+#define SOUND_THRESH 125.0
+
+/**
+ * @brief update the motor state with the current vector_arr data
+ */
+void updateMotors(){
+	if(vector_arr[0] > SOUND_THRESH){
+		set_motor(0,MOTOR_SPEED,true);
+		set_motor(1,MOTOR_SPEED,true);
+	}else if(vector_arr[0] < -SOUND_THRESH){
+		set_motor(0,MOTOR_SPEED,false);
+		set_motor(1,MOTOR_SPEED,false);
+	}else{
+		set_motor(0,0,true);
+		set_motor(1,0,true);
+	}
+}
+
+//#define JUST_FORWARD_DEBUG
+
 int main(){
 	Initialize_Board();
-	//Initialize_Motor();
+	Initialize_Motor();
+#ifndef JUST_FORWARD_DEBUG
 	Initialize_I2CADC();
 
 
-	printf("Board initialized\r\n");
+	printf("ROVR initialized\r\n");
 
+	updateMotors();
+	Motor_Enable();
 
-	//set_motor(0,64,true);
-	//set_motor(1,64,true);
-	//Motor_Enable();
+	bool fc = false;
+	float sample_avgs[NUM_MICS] = {0.0f,0.0f,0.0f,0.0f};
 
-
-	uint8_t fc = 0;
-	float sample_avgs[4] = {0.0f,0.0f,0.0f,0.0f};
-
-	while(1){
+	while(true){
 		uint8_t mux_num = 0;
-		for(;mux_num<4;++mux_num){
+		for(;mux_num<NUM_MICS;++mux_num){
 			sample_avgs[mux_num] = Read_Mic(mux_num);
 			if(fc){
 				vectorMath(sample_avgs);
-
-				//TODO : Use vector_differences to set PWM.
+				updateMotors();
 			}
 		}
 
-		fc = 1;
+		fc = true;
 	}
-		
+#else // JUST_FORWARD_DEBUG
+	set_motor(0,64,true);
+	set_motor(1,64,true);
+	Motor_Enable();
+
+	while(true) __WFI();
+#endif // JUST_FORWARD_DEBUG
 	return 0;
 }
+
