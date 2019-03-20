5356f5c863fde00d3f886e57d397c5440da783a8
gareyflee <46618024+gareyflee@users.noreply.github.com>
2019-03-19 19:16:39 -0700

    Update main.c
    Test_Code for Kyle's algorithm

--- main.c
+++ main.c
@@ -5,14 +5,30 @@
 #include "i2cadc.h"
 #include "motor.h"
 
+#define NUM_MICS 4
+
+
+
 
 #define MOTOR_SPEED 128
 #define SOUND_THRESH_FB 200.0
 #define SOUND_THRESH_LR 100.0
 #define MAX_LR_THRESH 500.0
 
+#define MIC_SCALAR_MAX 1000.0
+#define MIC0_SCALAR MIC_SCALAR_MAX/957.761875
+#define MIC1_SCALAR MIC_SCALAR_MAX/794.4513438
+#define MIC2_SCALAR MIC_SCALAR_MAX/802.3004688
+#define MIC3_SCALAR MIC_SCALAR_MAX/875.5912188
 
-#define NUM_SAMPLES 64
+#define NUM_SAMPLES 10
+#define NUM_SAMPLE_GROUPS 4
+#define NUM_SAMPLE_GROUP_HISTORY 2
+
+float MIC_SCALARS[] = {MIC0_SCALAR, MIC1_SCALAR, MIC2_SCALAR, MIC3_SCALAR};
+float Samples[NUM_MICS][NUM_SAMPLE_GROUP_HISTORY][NUM_SAMPLE_GROUPS];
+
+
 int WAIT_FB = 0;
 int WAIT_LR = 0;
 #define MAX_WAIT_TIME 512
@@ -33,11 +49,12 @@ void Initialize_Board(){
  * @return average power of mic signal over NUM_SAMPLES samples
  */
 float Read_Mic(uint8_t mux_num){
+	static uint8_t last_mux_num = -1;
 	int8_t* Sample_Data;
 	float sample_avg;
 	uint16_t bytes_processed = 0;
-	
-	Config_ADC(mux_num);
+	if (last_mux_num != mux_num)
+		Config_ADC(mux_num);
 	bytes_processed = 0;
 	sample_avg = 0.0;
 	Sample_Data = (int8_t*)Read_ADC(NUM_SAMPLES);
@@ -48,12 +65,21 @@ float Read_Mic(uint8_t mux_num){
 			sample_avg += converted*converted;
 		}
 	}
-	sample_avg /= NUM_SAMPLES;
-
+	sample_avg /= (NUM_SAMPLES*MIC_SCALARS[mux_num]);
+	last_mux_num = mux_num;
 	return sample_avg;
 }
 
-#define NUM_MICS 4
+void Add_Circle_Buffer(float* new_data, int mux_num){
+//	Only valid for NUM_SAMPLE_HISTORY of 2
+// float Samples[NUM_MICS][NUM_SAMPLE_GROUP_HISTORY][NUM_SAMPLE_GROUPS] = {0};
+	int i;
+	for (i=0; i < NUM_SAMPLE_GROUPS; i++){
+		Samples[mux_num][0][i] = Samples[mux_num][1][i];
+		Samples[mux_num][1][i] = new_data[i];
+	}
+	return;
+}
 
 //Forward/backward, right/left
 static float vector_arr[2] = {0.0f,0.0f};
@@ -154,39 +180,66 @@ void updateMotors(){
 // #define JUST_FORWARD_DEBUG
 
 int main(){
+	memset(Samples, 0.0, sizeof(Samples));
 	Initialize_Board();
 	Initialize_Motor();
-#ifndef JUST_FORWARD_DEBUG
+//#ifndef JUST_FORWARD_DEBUG
 	Initialize_I2CADC();
 
 
-	//printf("ROVR initialized\r\n");
-
-	updateMotors();
-	Motor_Enable();
-
+//	updateMotors();
+//	Motor_Enable();
+//float Samples[NUM_SAMPLES_GROUPS][NUM_SAMPLES] = {0};
 	bool fc = false;
+	int num_trials = 0;
 	float sample_avgs[NUM_MICS] = {0.0f,0.0f,0.0f,0.0f};
+	int Kyles_Sample_Num = 0;
+	float Kyles_Samples[4][NUM_MICS*NUM_SAMPLE_GROUPS*100];
+	memset(Kyles_Samples, 0.0, sizeof(Kyles_Samples));
+	int mux_num;
+	int sample_num;
 
-	while(true){
-		uint8_t mux_num = 0;
-		for(;mux_num<NUM_MICS;++mux_num){
-			sample_avgs[mux_num] = Read_Mic(mux_num);
-			if(fc){
-				vectorMath(sample_avgs);
-				updateMotors();
+	while(num_trials++ < 100){
+		for (mux_num = 0; mux_num < NUM_MICS; mux_num++){
+			for (sample_num = 0; sample_num < NUM_SAMPLE_GROUPS; sample_num++){
+				sample_avgs[sample_num] = Read_Mic(mux_num);
+				Kyles_Samples[mux_num][Kyles_Sample_Num++] = sample_avgs[sample_num];
+
 			}
+			Add_Circle_Buffer(sample_avgs, mux_num);
 		}
 
-		fc = true;
 	}
-#else // JUST_FORWARD_DEBUG
-	set_motor(0,255,true);
-	set_motor(1,255,true);
-	Motor_Enable();
+	int i;
+	int j;
+	for (j=0; j < 4; j++){
+		for (i=0; i < 1600; i ++){
+			printf("%f\r", Kyles_Samples[j][i]);
+		}
+		printf("***********************************\r\n\n");
+	}
 
-	while(true) __WFI();
-#endif // JUST_FORWARD_DEBUG
+
 	return 0;
+//	while(true){
+//		uint8_t mux_num = 0;
+//		for(;mux_num<NUM_MICS;++mux_num){
+//			sample_avgs[mux_num] = Read_Mic(mux_num);
+//			if(fc){
+//				vectorMath(sample_avgs);
+//				printf("F/B: %f\r\n", vector_arr[0]);
+////				updateMotors();
+//			}
+//		}
+//
+//		fc = true;
+//	}
+////#else // JUST_FORWARD_DEBUG
+////	set_motor(0,255,true);
+////	set_motor(1,255,true);
+////	Motor_Enable();
+////
+////	while(true) __WFI();
+////#endif // JUST_FORWARD_DEBUG
+//	return 0;
 }
-
